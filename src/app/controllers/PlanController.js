3a331958d5566f8dd886679b5971c9911a2dbae1
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2019-10-19 19:36:46 -0300

    feature: Gestao de planos

---
+++ src/app/controllers/PlanController.js
@@ -0,0 +1,117 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
+class PlanController {
+  async store(req, res) {
+    /**
+     * Fields validation
+     */
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number()
+        .integer()
+        .moreThan(0)
+        .required(),
+      price: Yup.number()
+        .positive()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    /**
+     * check if plan name is already in use
+     */
+
+    const planNameExists = await Plan.findOne({
+      where: { title: req.body.title },
+    });
+
+    if (planNameExists) {
+      return res.status(400).json({ error: 'Plan name is already in use' });
+    }
+
+    const { id, title, duration, price } = await Plan.create(req.body);
+
+    return res.json({
+      id,
+      title,
+      duration,
+      price,
+    });
+  }
+
+  async index(req, res) {
+    const plan = await Plan.findAll({
+      attributes: ['id', 'title', 'duration', 'price'],
+      order: ['price'],
+    });
+
+    return res.json(plan);
+  }
+
+  async update(req, res) {
+    /**
+     * Fields validation
+     */
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number()
+        .integer()
+        .moreThan(0),
+      price: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    /**
+     * check if plan exists
+     */
+    const { planId } = req.params;
+
+    const plan = await Plan.findByPk(planId);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exists' });
+    }
+
+    /**
+     * check if plan name is already in use
+     */
+    const { title } = req.body;
+
+    if (title !== plan.title) {
+      const planNameExists = await Plan.findOne({
+        where: { title },
+      });
+
+      if (planNameExists) {
+        return res.status(400).json({ error: 'Plan name is already in use' });
+      }
+    }
+
+    const { id, duration, price } = await plan.update(req.body);
+
+    return res.json({ id, title, duration, price });
+  }
+
+  async delete(req, res) {
+    const { planId } = req.params;
+
+    const plan = await Plan.findByPk(planId);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exists' });
+    }
+
+    plan.destroy();
+
+    return res.json({ sucess: 'Deleted' });
+  }
+}
+
+export default new PlanController();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,10 @@ routes.use(authMiddleware);
 
 routes.post('/students', StudentController.store);
 routes.put('/students', StudentController.update);
+
+routes.post('/plans', PlanController.store);
+routes.get('/plans', PlanController.index);
+routes.put('/plans/:planId', PlanController.update);
+routes.delete('/plans/:planId', PlanController.delete);
 
 export default routes;
