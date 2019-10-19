681ee7358d321cb2328ea5a6b3a00f99d3307108
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2019-10-19 16:18:55 -0300

    feature: Persistencia de student

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,6 +1,96 @@
+import * as Yup from 'yup';
+
+import Student from '../models/Student';
+
 class StudentController {
   async store(req, res) {
-    return res.json({ res: ' Student Controller ' });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      idade: Yup.number()
+        .integer()
+        .positive()
+        .required(),
+      peso: Yup.number()
+        .integer()
+        .positive()
+        .required(),
+      altura: Yup.number()
+        .integer()
+        .positive()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+
+    const emaiExists = await Student.findOne({ where: { email } });
+
+    if (emaiExists) {
+      return res.status(400).json({ error: 'This email is already in use' });
+    }
+
+    const { id, name, idade, peso, altura } = await Student.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      idade,
+      peso,
+      altura,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      idade: Yup.number()
+        .integer()
+        .positive(),
+      peso: Yup.number()
+        .integer()
+        .positive(),
+      altura: Yup.number()
+        .integer()
+        .positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id, email } = req.body;
+
+    const student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(401).json({ error: 'Student not found' });
+    }
+
+    if (email && email !== student.email) {
+      const emaiExists = await Student.findOne({ where: { email } });
+
+      if (emaiExists) {
+        return res.status(400).json({ error: 'This email is already in use' });
+      }
+    }
+
+    const { name, idade, peso, altura } = await student.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      idade,
+      peso,
+      altura,
+    });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ const routes = new Router();
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
 
 export default routes;
